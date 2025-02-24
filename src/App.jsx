e103455c99f8065c842a9171111709c6d2c4eaee
Aladdin Alizada <tu7c5zneb@code.edu.az>
2025-02-25 02:44:37 +0400

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./layout/footer";
+import Header from "./layout/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div>
+          <p>azcargo.az</p>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
