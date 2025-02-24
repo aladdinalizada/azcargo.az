7855aa8c1c851195599b59d69cfbd98b3ba0beb2
Aladdin Alizada <tu7c5zneb@code.edu.az>
2025-02-25 03:24:28 +0400

    complete Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./layout/footer";
 import Header from "./layout/header";
+import Contact from "./pages/contact";
+import Home from "./pages/Home";
+import Services from "./pages/services";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
       <Header />
-      <main>
-        <div>
-          <p>azcargo.az</p>
-        </div>
-      </main>
+      <Helmet>
+        <title>AZCARGO</title>
+      </Helmet>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/layout/header/index.jsx
+++ src/layout/header/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+import "./index.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="headerContainer">
+      <Link to="/">
+        <img src={Logo} alt="" className="logo" />
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
